3941e29892500121e488394238fa53f9a3a0b90f
UzairIshfaq1234 <uzairishfaq1234@gmail.com>
2022-08-20 12:06:01 +0500

    str function

--- PHP2.php
+++ PHP2.php
@@ -1,15 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP-2</title>
 </head>
+
 <body>
     <div class="container">
-        <h1>Muhammad Uzair Ishfaq</h1>
-        <h2>Zainb Pagal he</h2>
-    </div>    
+        <!-- <h1>Muhammad Uzair Ishfaq</h1>
+        <h2>Zainb Pagal he</h2> -->
+        <?php
+        // echo"Muhammad Uzair";
+        $student="Uzair Ishfaq";
+        // -----------------8-AUG-2022---------------
+        // ----------------STRING  FUNCTION--------
+        // var_dump($student);
+        // echo strlen($student);
+        // echo str_word_count($student);
+
+
+
+
+
+
+        ?>
+    </div>
 </body>
+
 </html>
